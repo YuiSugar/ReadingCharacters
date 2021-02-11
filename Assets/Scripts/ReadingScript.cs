a8883fd3bbe395df948a2253f11c377e7fddff81
Yui Sugar <ysato444@gmail.com>
2021-02-11 21:39:54 +0900

    add Reading Characters Behaviour Script

---
+++ Assets/Scripts/ReadingScript.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReadingScript : MonoBehaviour
+{
+    // ancient monument is hitted by avatar
+    bool isHitAvatar = false;
+    // avatar object
+    GameObject monument;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P) && monument != null)
+        {
+            AudioSource audioSource = monument.GetComponentInChildren<AudioSource>();
+            audioSource.volume = 1.0F;
+            audioSource.Play(0);
+            monument = null;
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        UnityEngine.Debug.Log("Hit");
+        GameObject hittedObject = collision.gameObject;
+        string hittedObjectName = hittedObject.name;
+        UnityEngine.Debug.Log("Hit at: " + hittedObjectName);
+        if (hittedObjectName.Equals("Behistun_relief") ||
+            hittedObjectName.Equals("Rosetta_Stone")) 
+        { 
+            isHitAvatar = true;
+            monument = hittedObject;
+        } 
+        else 
+        {
+            isHitAvatar = false;
+            monument = null;
+        }
+    }
+}
